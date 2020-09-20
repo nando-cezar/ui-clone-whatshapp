371a1c4af6254d14e4d66f8a26bdc6c50f2650a2
Luis Fernando Cezar dos Santos <luisfernando_cezar@hotmail.com>
2020-09-19 21:35:29 -0300

    whatshapp | project init;

---
+++ whatshapp/src/App.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+import ChatListItem from './components/ChatListItem'
+
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+
+export default () => {
+
+  const [chatlist, setChatList] = useState([{},{},{},{}]);
+
+  return(
+    <div className="app-window">
+        <div className="sidebar">
+            <header>
+              <img className="header--avatar" src="https://www.w3schools.com/w3images/avatar2.png" alt=""/>
+              <div className="header--buttons">
+                <div className="header--btn">
+                  <DonutLargeIcon style={{color:'#919191'}}/>
+                </div>
+                <div className="header--btn">
+                  <ChatIcon style={{color:'#919191'}}/>
+                </div>
+                <div className="header--btn">
+                  <MoreVertIcon style={{color:'#919191'}}/>
+                </div>
+              </div>
+            </header>
+            <div className="search">
+              <div className="search--input">
+                <SearchIcon fontSize="small" style={{color:'#919191'}}/>
+                <input type="search" placeholder="Procurar ou começar uma nova conversa."/>
+              </div>
+            </div>
+            <div className="chatlist">
+              {chatlist.map((item, key)=>(
+                <ChatListItem 
+                  key={key}
+                />
+              ))}
+            </div>
+        </div>
+        <div className="contentarea">
+          ...
+        </div>
+    </div>
+  );
+}
+
