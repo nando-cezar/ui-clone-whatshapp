0b29f464a3cd8f016d725ec20984b3302d7a9242
veyson <luisfernando_cezar@hotmail.com>
2020-12-11 17:20:11 -0300

    react-whatshapp | integration firebase;

---
+++ whatshapp/src/firebaseConfig.js
@@ -0,0 +1,9 @@
+
+export default {
+  apiKey: "AIzaSyDIM9ltEJ1aqB6PP5UxBY2BOOdjgN0Eq8A",
+  authDomain: "react-whatshapp.firebaseapp.com",
+  projectId: "react-whatshapp",
+  storageBucket: "react-whatshapp.appspot.com",
+  messagingSenderId: "854924491332",
+  appId: "1:854924491332:web:d6c3b9846efc297e8a9a11"
+};
